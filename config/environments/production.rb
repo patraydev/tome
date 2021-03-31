3c277063c17a42b74d5742adce66ff518bfdf1ef
Pat Ray <patraydev@gmail.com>
2021-03-31 17:48:47 -0500

    devise before initial migrate

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Mine setting up devise
+  config.action_mailer.default_url_options = { host: 'http://your-app-on-heroku.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: Rails.application.credentials.email,
+      password: Rails.application.credentials.email_password
+
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
