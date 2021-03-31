cc3bc52920b143f48918a5fd927d956a4643bb4a
Pat Ray <patraydev@gmail.com>
2021-03-31 18:56:12 -0500

    foreign keys defined?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   belongs_to :program
-  has_and_belongs_to_many :cocktails
+  has_and_belongs_to_many :cocktails, foreign_key: 'cocktail_id'
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable,  and :omniauthable
   devise :database_authenticatable, :registerable, :trackable,
