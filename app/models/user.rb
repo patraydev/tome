d612cbfc99119d51892c106c36dc4f4906c39ff3
Pat Ray <patraydev@gmail.com>
2021-03-31 18:00:57 -0500

    scaffolded out Program and Cocktail tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable,  and :omniauthable
   devise :database_authenticatable, :registerable, :trackable,
