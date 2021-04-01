efa2f5e61cbb06b59f2d2325007ad9a223bbbef1
Pat Ray <patraydev@gmail.com>
2021-03-31 19:30:32 -0500

    installed db_cleaner gem and some seeds boilerplat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+DatabaseCleaner.clean_with(:truncation)
+
+
+@admin = User.create!(username: 'admin', email: 'admin@email.com', password: '123456')
+
+puts "#{User.count} users created"
+
+@salty = Flavor.create!(name: 'salty')
+@sweet = Flavor.create!(name: 'sweet')
+@sour = Flavor.create!(name: 'sour')
+@bitter = Flavor.create!(name: 'bitter')
+@umami = Flavor.create!(name: 'umami')
+
+puts "#{Flavor.count} flavors created"
+
+Food.create!(name: 'pizza', user: @admin, flavors: [@salty, @sweet, @umami])
+@ice_cream = Food.create!(name: 'ice cream', user: @admin)
+
+@ice_cream.flavors.push(@sweet, @salty)
+
+@tacos = Food.create!(name: 'tacos', user: @admin)
+
+@salty.foods << @tacos
+@sour.foods << @tacos
+@umami.foods << @tacos
+
+puts "#{Food.count} foods created"
