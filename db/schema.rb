4b53c8e04eec71a2541f565638244de826e41e26
Pat Ray <patraydev@gmail.com>
2021-04-01 12:02:39 -0500

    IT SEEDS

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_233621) do
+ActiveRecord::Schema.define(version: 2021_04_01_165028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,11 @@ ActiveRecord::Schema.define(version: 2021_03_31_233621) do
     t.string "request_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "garnish"
+    t.string "float"
+    t.string "top"
+    t.bigint "program_id"
+    t.index ["program_id"], name: "index_cocktails_on_program_id"
   end
 
   create_table "libraries", id: false, force: :cascade do |t|
@@ -62,7 +67,21 @@ ActiveRecord::Schema.define(version: 2021_03_31_233621) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer "invitation_limit"
+    t.string "invited_by_type"
+    t.bigint "invited_by_id"
+    t.integer "invitations_count", default: 0
+    t.boolean "is_admin", default: false, null: false
+    t.bigint "program_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
+    t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
+    t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by"
+    t.index ["program_id"], name: "index_users_on_program_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
