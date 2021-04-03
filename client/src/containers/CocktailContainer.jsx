c67fbcd4d24daf1e1a7c39b867d096d6dc6f7727
Pat Ray <patraydev@gmail.com>
2021-04-02 20:53:57 -0500

    got through search component

--- client/src/containers/CocktailContainer.jsx
+++ client/src/containers/CocktailContainer.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from "react-router-dom";
+
+import Modal from "../components/Modal.jsx";
+import CocktailDetail from "../screens/SearchResults.jsx";
 
 import {
   readAllCocktails,
@@ -12,19 +15,51 @@ import circle from "../assets/images/circle.png";
 import "../assets/style/CocktailContainer.css";
 
 function CocktailContainer(props) {
-  const [] = useState();
-  useEffect(() => {}, []);
+  const [cocktails, setCocktails] = useState([]);
+  const history = useHistory();
+  const { currentUser } = props;
 
-  return (
-      <div className="cocktail-container">
-        <div
-          className="weird-circle"
-          style={{ backgroundImage: `url(${circle})` }}
-        >
-        
-        </div>
-      </div>
-  );
+  useEffect(() => {
+    const fetchCocktails = async () => {
+      const result = await readAllTCocktails();
+      setCocktails(result);
+    };
+    fetchCocktails();
+  }, []);
+
+  // const newCocktail = async (formData) => {
+  //   const newCocktail = await createCocktail(formData);
+  //   setCocktails((prevState) => [...prevState, newCocktail]);
+  //   history.push("/cocktails");
+  // };
+
+  // const editCocktail = async (id, formData) => {
+  //   const updatedCocktail = await updateCocktail(id, formData);
+  //   setCocktails((prevState) =>
+  //     prevState.map((cocktail) => {
+  //       return cocktail.id === Number(id) ? updatedCocktail : cocktail;
+  //     })
+  //   );
+  //   history.push(`/${id}`);
+  // };
+  
+  // const deleteCocktail = async (id) => {
+  //   await destroyCocktail(id);
+  //   setCocktails((prevState) =>
+  //     prevState.filter((cocktail) => {
+  //       return cocktail.id !== id;
+  //     })
+  //   );
+  //   history.push("/cocktails");
+  // };
+
+  return <Search cocktails={cocktails}/>;
 }
 
 export default CocktailContainer;
+
+
+
+
+
+
