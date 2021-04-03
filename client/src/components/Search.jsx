700ebb7ef3c556b883d68247c2939ddb9652a847
Pat Ray <patraydev@gmail.com>
2021-04-02 20:53:57 -0500

    got through search component

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,36 +1,44 @@
-import {useState,useEffect } from "react";
+import { useState } from "react";
 
-function Search(props) {
-const [] = useState();
-useEffect(() => {
+import { showModal, hideModal } from "../helpers/modal.js";
+import SearchResults from "../screens/SearchResults.jsx";
 
-},[]);
+function Search({cocktails}) {
+  const [show, setShow] = useState(false);
+
+  const [formData, setFormData] = useState({ searchTerm: "" });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   return (
     <div className="search-main">
-    <Modal show={show} handleClose={hideModal}>
-      <Login handleLogin={handleLogin} />
-    </Modal>
-    <button onClick={showModal}>login</button>
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleSearch(formData);
-      }}
-    >
-      <label>
-        Username:
-        <input
-          name="email"
-          type="text"
-          value={username}
-          onChange={handleChange}
-        />
-      </label>
-      <button type="submit">Search</button>
-    </form>
-  </div>
-);
-};
+      <Modal show={show} handleClose={hideModal}>
+        <SearchResults searchTerm={formData.searchTerm} cocktails={cocktails}/>
+      </Modal>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          showModal();
+        }}
+      >
+        <label htmlFor="search">
+          <input
+            name="searchTerm"
+            type="text"
+            value={searchTerm}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+}
 
-export default Search
+export default Search;
