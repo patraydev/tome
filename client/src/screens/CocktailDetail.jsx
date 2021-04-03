91e5abf7bfa311e0a121f74f7647626a51945d30
Pat Ray <patraydev@gmail.com>
2021-04-02 20:32:03 -0500

    login works, building main container

---
+++ client/src/screens/CocktailDetail.jsx
@@ -0,0 +1,12 @@
+import {useState,useEffect } from "react";
+
+function CocktailDetail(props) {
+const [] = useState();
+useEffect(() => {
+
+},[]);
+
+return ();
+};
+
+export default CocktailDetail
