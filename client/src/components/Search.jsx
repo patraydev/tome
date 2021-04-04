9c336312250384f495c398dc36214e42065d323d
Pat Ray <patraydev@gmail.com>
2021-04-04 16:38:20 -0500

    cocktails are displaying!

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 import SearchResults from "../screens/SearchResults.jsx";
-import Modal from './Modal.jsx';
+import Modal from "./Modal.jsx";
 
-import '../assets/style/Search.css';
+import "../assets/style/Search.css";
 
-function Search({cocktails}) {
+function Search({ cocktails }) {
   const [show, setShow] = useState(false);
 
   const [formData, setFormData] = useState({ searchTerm: "" });
@@ -19,33 +19,37 @@ function Search({cocktails}) {
     }));
   };
 
-   const showModal = () => {
+  const showModal = () => {
     setShow(true);
   };
-  
-   const hideModal = () => {
+
+  const hideModal = () => {
     setShow(false);
   };
 
-
   return (
     <div className="search-main">
-      <Modal show={show} handleClose={hideModal} size='large'>
-        <SearchResults searchTerm={formData.searchTerm} cocktails={cocktails}/>
+      <Modal show={show} handleClose={hideModal} size="large">
+        <SearchResults
+          searchTerm={formData.searchTerm}
+          cocktails={cocktails}
+          title="Search Results"
+        />
       </Modal>
-      <form className='search-form'
+      <form
+        className="search-form"
         onSubmit={(e) => {
           e.preventDefault();
           showModal();
         }}
       >
         <label htmlFor="search"></label>
-          <input
-            name="searchTerm"
-            type="text"
-            value={searchTerm}
-            onChange={handleChange}
-          />
+        <input
+          name="searchTerm"
+          type="text"
+          value={searchTerm}
+          onChange={handleChange}
+        />
         <button type="submit">Search</button>
       </form>
     </div>
