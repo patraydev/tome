f595c2a7728ab7ed38b5d4b2ad71025bfc1221bf
Pat Ray <patraydev@gmail.com>
2021-04-04 16:38:20 -0500

    cocktails are displaying!

--- client/src/screens/SearchResults.jsx
+++ client/src/screens/SearchResults.jsx
@@ -1,12 +1,32 @@
 import {useState,useEffect } from "react";
 
-function SearchResults(props) {
+import '../assets/style/SearchResults.css';
+
+
+function SearchResults({searchTerm,title,cocktails}) {
 const [] = useState();
 useEffect(() => {
 
 },[]);
 
-return (<>=P</>);
+  const filteredCocktails = cocktails.map((cocktail) => {
+    cocktail.name.includes(searchTerm)  
+}) 
+  
+const list = cocktails.map((cocktail, index) =>
+  <div className='list-item' key={index}>
+    {cocktail.name}
+    </div>
+)
+  
+  return (
+    <div className='results-container'>
+      <div className='title'>{`${title}`}</div>
+      <div className='results-list'>
+        {list}
+      </div>
+    </div>
+  );
 };
 
 export default SearchResults
