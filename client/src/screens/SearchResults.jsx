755f6997e1ba76b62551ea59cc45f475b6ae91c9
Pat Ray <patraydev@gmail.com>
2021-04-05 16:26:34 -0500

    grid styling and scrollbar removal services llc

--- client/src/screens/SearchResults.jsx
+++ client/src/screens/SearchResults.jsx
@@ -17,23 +17,32 @@ function SearchResults({ searchTerm, title, cocktails }) {
   const handleSelect = (e) => {
     e.preventDefault();
     console.log(typeof e.target.id);
-    setDisplayCocktail(cocktails.find(cocktail => cocktail.id === parseInt(e.target.id)));
-}
+    setDisplayCocktail(
+      cocktails.find((cocktail) => cocktail.id === parseInt(e.target.id))
+    );
+  };
 
   const list =
     filteredCocktails &&
     filteredCocktails.map((cocktail, index) => (
-      <div className="list-item" key={index} id={cocktail.id} onClick={(e)=>handleSelect(e)}>
+      <div
+        className="list-item"
+        key={index}
+        id={cocktail.id}
+        onClick={(e) => handleSelect(e)}
+      >
         {cocktail ? cocktail.name : "nothing found"}
       </div>
     ));
 
   return (
     <div className="results-container">
-      <div className="title">{`${title}`}</div>
-      <div className="results-list">{list}</div>
-      <div className='display'>cocktail goes here</div>
-      <div className='detail-button-container'>
+      {/* <div className="results-list-container"> */}
+        <div className="title">{`${title}`}</div>
+        <div className="results-list">{list}</div>
+      {/* </div> */}
+      <div className="display">cocktail goes here</div>
+      <div className="detail-button-container">
         <button>Edit</button>
         <button>Add to Library</button>
       </div>
