312582934a95e9370ededc3f4cc6f19b37bf8668
Pat Ray <patraydev@gmail.com>
2021-04-06 05:16:25 -0500

    mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Mine setting up devise
-  config.action_mailer.default_url_options = { host: 'http://your-app-on-heroku.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://tome-api.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
