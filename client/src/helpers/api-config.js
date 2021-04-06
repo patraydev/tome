d7c7c5d2cf8c9a5c23c1793d3066c5f4db650f7b
Pat Ray <patraydev@gmail.com>
2021-04-06 04:44:29 -0500

    added heroku config url var

--- client/src/helpers/api-config.js
+++ client/src/helpers/api-config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000'
+const baseUrl = REACT_APP_API_URL || 'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl,
