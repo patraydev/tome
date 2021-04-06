ffee157be4ff5c20c5b904810b1d0283243c6526
Pat Ray <patraydev@gmail.com>
2021-04-06 06:11:59 -0500

    200

--- client/src/helpers/api-config.js
+++ client/src/helpers/api-config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000'
+const baseUrl = 'tome-api.herokuapp.com' 
 
 const api = axios.create({
   baseURL: baseUrl,
